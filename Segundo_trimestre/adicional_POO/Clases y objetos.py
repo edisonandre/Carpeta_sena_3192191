dc760892a5fc080ebd7d539a52c16dcf2471a884
edison andres martinez <edimartinezposso@gmail.com>
2025-08-25 19:57:47 -0500

    adicional

--- Segundo_trimestre/adicional_POO/Clases y objetos.py
+++ Segundo_trimestre/adicional_POO/Clases y objetos.py
@@ -1,25 +1,25 @@
-# class celular():
-#     marca = "samsung" #objetos
-#     modelo = "s24"
-#     camara = "28MP"
+class celular():
+    marca = "samsung" #objetos
+    modelo = "s24"
+    camara = "28MP"
 
-# celular1 = celular()  #objeto de la instacia de celular
-# celular2 = celular()
-# celular3 = celular()
-# celular4 = celular()
+celular1 = celular()  #objeto de la instacia de celular
+celular2 = celular()
+celular3 = celular()
+celular4 = celular()
 
-# print(celular3.modelo)
+print(celular2.modelo)
 
 #variables que pertenece a una clase
 
-# class celular():
-#     marca = "samsung" #objetos
-#     modelo = "s24"
-#     camara = "28MP"
+class celular():
+    marca = "samsung" #objetos
+    modelo = "s24"
+    camara = "28MP"
     
-# celular1 = celular()  #objeto de la instacia de celular
-# celular.marca = "Apple" #no es optimo
-# print(celular1.marca)
+celular1 = celular()  #objeto de la instacia de celular
+celular.marca = "Apple" #no es optimo
+print(celular1.marca)
 
 class celular:          #valor a pasar
     def __init__(self, marca, modelo, camara): #(self) hace referencia asi mismo
@@ -27,9 +27,45 @@ class celular:          #valor a pasar
         self.mar = marca #marca es propiedad de self
         self.modelo = modelo #modelo es propiedad de self
         self.camara = camara #camara es propiedad de self
+        
+    def llamar(self):
+        print(f"estas haciendo un llamado desde un: {self.modelo}")
+        
+    def cortar(self):
+        print(f"cortaste la llamada desde tu: {self.modelo}")
 
 celular1 = celular("samsung", "s24", "48MP")#guarda estos dato a clase
 celular2 = celular("Honor", "x7a", "56MP")
-print(celular1.mar)
-print(celular2.modelo)
+
+celular1.cortar() #se ejecuta sin un print
+
+
+#ejercico edad clase y grado
+class Estudiante:
+    def __init__(self, nombre, Edad, Grado):
+        self.nombre = nombre
+        self.edades = Edad
+        self.grado = Grado
+        
+    def estudiar(self):
+        print("estudiando...")
+        
+nombre = input("Digame su nombre ")
+edad = input("ingrese su edad ")
+grado = input("ingrese su grado ")
+
+estudiante = Estudiante(nombre, edad, grado)
+
+print(f"""
+    DATOS DEL ESTUDIANTE: \n\n
+    Nombre: {estudiante.nombre} \n
+    Edad: {estudiante.edades} \n
+    Grado: {estudiante.grado}  \n
+    """)
+
+while True:
+    estudiar = input()
+    if (estudiar.lower() == "estudiar"):
+        estudiante.estudiar()
+
 
